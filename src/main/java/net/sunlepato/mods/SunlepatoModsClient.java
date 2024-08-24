fef45b4244b8d5ab567488cc2ebc54d8554343ff
Sunlepato <164724412+Sunlepato@users.noreply.github.com.>
2024-08-24 16:47:35 -0500

    initial commit

---
+++ src/main/java/net/sunlepato/mods/SunlepatoModsClient.java
@@ -0,0 +1,10 @@
+package net.sunlepato.mods;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SunlepatoModsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/sunlepato/mods/SunlepatoModsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.sunlepato.mods;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SunlepatoModsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
